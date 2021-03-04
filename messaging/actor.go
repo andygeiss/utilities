603560b6a73c28cfc103a547991d9d0705cce481
andygeiss <4ndygeiss@gmail.com>
2021-03-04 13:12:47 +0100

    send added

--- messaging/actor.go
+++ messaging/actor.go
@@ -3,4 +3,5 @@ package messaging
 // Actor defines a simple interface to ensure that each actor is able to choose and handle relevant messages by itself.
 type Actor interface {
 	Receive(message interface{})
+	Send(message interface{})
 }
--- messaging/bus_test.go
+++ messaging/bus_test.go
@@ -19,6 +19,10 @@ func (a *ActorStub) Receive(message interface{}) {
 	}
 }
 
+func (a *ActorStub) Send(message interface{}) {
+	a.Bus.Publish(message)
+}
+
 type MessageStub struct{}
 
 func TestBusPublishAfterSubscribe(t *testing.T) {
@@ -43,6 +47,17 @@ func TestTwoActors(t *testing.T) {
 	bus.Subscribe(actor1)
 	bus.Subscribe(actor2)
 	bus.Publish(MessageStub{})
+	assert.That("state of actor1 should be changed", t, actor1.StateChanged, true)
+	assert.That("state of actor2 should be changed", t, actor2.StateChanged, true)
+}
+
+func TestActorSend(t *testing.T) {
+	bus := messaging.NewBus()
+	actor1 := &ActorStub{Bus: bus}
+	actor2 := &ActorStub{Bus: bus}
+	bus.Subscribe(actor1)
+	bus.Subscribe(actor2)
+	actor1.Send(MessageStub{})
 	assert.That("state of actor1 should be changed", t, actor1.StateChanged, true)
 	assert.That("state of actor2 should be changed", t, actor2.StateChanged, true)
 }
