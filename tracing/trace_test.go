7ae5154178ab6287704b061ee3ddd83effa708ff
andygeiss <4ndygeiss@gmail.com>
2021-03-12 14:09:49 +0100

    test added

--- tracing/trace_test.go
+++ tracing/trace_test.go
@@ -36,3 +36,21 @@ source -> target: label
 	assert.That("trace2 should not be nil", t, trace2 != nil, true)
 	assert.That("out should be like wanted", t, out, wanted)
 }
+
+func TestToAndFromContextTwice(t *testing.T) {
+	trace := tracing.NewTrace()
+	trace.Add(tracing.NewSpan("source", "target", "label", time.Second))
+	ctx := trace.ToContext(context.Background())
+	trace2 := tracing.FromContext(ctx)
+	trace2.Add(tracing.NewSpan("source2", "target2", "label2", time.Second))
+	out := trace2.ToPlantUML()
+	wanted := `@startuml
+source -> target: label
+source2 -> target2: label2
+@enduml
+`
+	assert.That("trace should not be nil", t, trace != nil, true)
+	assert.That("trace context should not be nil", t, ctx != nil, true)
+	assert.That("trace2 should not be nil", t, trace2 != nil, true)
+	assert.That("out should be like wanted", t, out, wanted)
+}
