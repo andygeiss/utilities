385f74ccd6e394ab33076517c60841dd64dc6aa9
andygeiss <4ndygeiss@gmail.com>
2021-03-27 11:14:51 +0100

    logging added

---
+++ logging/request.go
@@ -0,0 +1,11 @@
+package logging
+
+import (
+	"log"
+	"net/http"
+)
+
+// Request ...
+func Request(r *http.Request, rid string, request interface{}, err error) {
+	log.Printf("[%-8s] [%-20s] [%s] request [%v] error [%v]", r.Method, r.RequestURI, rid, request, err)
+}
---
+++ logging/response.go
@@ -0,0 +1,12 @@
+package logging
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// Response ...
+func Response(r *http.Request, rid string, response interface{}, err error, start time.Time) {
+	log.Printf("[%-8s] [%-20s] [%s] response [%v] round trip [%v] error [%v]", r.Method, r.RequestURI, rid, response, time.Since(start), err)
+}
